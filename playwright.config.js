287c3f591ce2f920ce4860e701e5ac6ac0455516
cryzalis <dmytromaslo@gmail.com>
2022-09-22 11:04:55 +0300

    ci

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,23 @@
-// playwright.config.js
 // @ts-check
 
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
+const config = {
+  use: {
+    headless: false,
+    viewport: { width: 1280, height: 720 },
+    channel: 'chrome',
+    ignoreHTTPSErrors: true,
+    video: 'on-first-retry',
+    
+  },
+  reporter: "allure-playwright"
+
+};
+
+module.exports = config;
+
+/**
+
 const config = {
   timeout: 30000,
   globalTimeout: 600000,
@@ -10,3 +26,4 @@ const config = {
 };
 
 module.exports = config;
+ */
